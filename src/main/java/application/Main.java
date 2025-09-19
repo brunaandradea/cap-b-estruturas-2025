05dcc6888aa88d03a00e451bd5d0005041e58e6e
Bruna Andrade Alves <179890@unifio.edu.br>
2025-09-19 01:30:20 +0000

    Aula 18.09.2025

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,8 +1,18 @@
 package application;
 
+import application.lista.ListaSimples;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        ListaSimples lista = new ListaSimples();
+
+        lista.adicionar("Primeiro");
+        lista.adicionar("Segundo");
+        lista.adicionar("Terceiro");
+
+        lista.remover(1);
+
+        System.out.println(lista);
     }
 }
 
